cc9ccda093d523ce4dab6e0059e56326e1743ba9
bleeeeek <mame0009@student.monash.edu>
2025-01-30 19:53:03 +0800

    fixed bug 12

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/VoG-5.0-itinerary/',
+  base: './',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
